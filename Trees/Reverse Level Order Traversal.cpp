49cbf498b713de790a4dc8de78fe53084ab34b14
Rahul Agarwal <rahul123agarwal99@gmail.com>
2022-07-31 10:36:24 +0530

    Create Reverse Level Order Traversal.cpp

---
+++ Trees/Reverse Level Order Traversal.cpp
@@ -0,0 +1,20 @@
+vector<int> reverseLevelOrder(Node *root)
+{
+    // code here
+    vector<int>v;
+    queue<Node *>q;
+    q.push(root);
+    
+    while(q.empty() == false)
+    {
+        Node *curr = q.front();
+        q.pop();
+        v.push_back(curr->data);
+        
+        if(curr->right != NULL)q.push(curr->right);
+        if(curr->left != NULL)q.push(curr->left);
+        
+    }
+    reverse(v.begin(),v.end());
+    return v;
+}
