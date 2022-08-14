e9a9a3aeef614d290d74e56794de87c04d9a3053
Rahul Agarwal <rahul123agarwal99@gmail.com>
2022-08-14 15:39:51 +0530

    Create 3. Insert in a BST.cpp

---
+++ BST/3. Insert in a BST.cpp
@@ -0,0 +1,16 @@
+//recursive code
+
+Node* insert(Node* root, int Key) {
+    // Your code here
+    
+    if(root==NULL)
+    return new Node(Key);
+    
+    else if(root->data > Key)
+    root->left = insert(root->left,Key);
+    else if(root->data < Key)
+    root->right = insert(root->right,Key);
+    
+    return root;
+    
+}
