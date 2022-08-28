b89fa96fbeb04f8fbaa6bfd17b80d9b40d0a0aa6
Rahul Agarwal <rahul123agarwal99@gmail.com>
2022-08-28 12:22:50 +0530

    Create Delete last Node in a Linked List.cpp

---
+++ Linked List/Delete last Node in a Linked List.cpp
@@ -0,0 +1,17 @@
+Node *delLast(Node *head)
+{
+  if(head == NULL) return NULL;
+  if(head -> next == NULL)
+  {
+    delete(head);
+    return NULL;
+  }
+  Node *curr = head;
+  while(curr ->next->next != NULL)
+    curr = curr->next;
+  
+  delete(curr->next);
+  curr->next = NULL;
+  
+  return head;
+}
